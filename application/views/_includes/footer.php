dd76ad64701438a71b933601866d78fc5697155c
Josh Knight <josh@seven-seventeen.com>
2012-10-03 21:49:11 -0400

    Added google analytics to footer.

--- application/views/_includes/footer.php
+++ application/views/_includes/footer.php
@@ -10,8 +10,20 @@
 					<a href="<?php echo base_url(); ?>privacy_policy/5">Privacy Policy</a>
 				</div>
 			</footer>
-		</div>  
+		</div> 
 	</body>
+	
+	<script type="text/javascript">
+  		var _gaq = _gaq || [];
+  		_gaq.push(['_setAccount', 'UA-331855-36']);
+  		_gaq.push(['_trackPageview']);
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  		})();
+	</script>
+
 	<script>
 		$('input').toggleField({textsrc:"title"});
 	</script>
